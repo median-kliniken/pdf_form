7eb812b953f1619fe02e67b4e26b24a15662bb96
Markus Mayer <widemeadows@gmail.com>
2025-07-22 18:06:25 +0200

    Fix combobox text encoding

--- src/lib.rs
+++ src/lib.rs
@@ -782,17 +782,15 @@ impl Form {
                 options, editable, ..
             } => {
                 if options.contains(&choice) || editable {
-                    // Inline the string directly in PDFDocEncoding
-                    let value_obj = Object::String(choice.as_bytes().to_vec(), lopdf::StringFormat::Literal);
-
-                    // Set /V and /DV directly, removing old AP
+                    let value_obj = encode_pdf_string(&choice);
+                    
                     if let Some(Object::Dictionary(field)) = self.doc.objects.get_mut(&self.form_ids[n]) {
                         field.set("V", value_obj.clone());
                         field.set("DV", value_obj);
                         field.remove(b"AP");
                     }
 
-                    self.regenerate_choice_appearance(n).map_err(|_| ValueError::InvalidSelection)?;
+                    // self.regenerate_choice_appearance(n).map_err(|_| ValueError::InvalidSelection)?;
                     Ok(())
                 } else {
                     Err(ValueError::InvalidSelection)
